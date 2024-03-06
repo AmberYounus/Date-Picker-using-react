712e5c8340b78daf7df574a0760006cb738c89cf
Amber Younus <amberyounus22@gmail.com>
2024-03-05 22:59:14 -0800

    commit

--- src/DatePicker.js
+++ src/DatePicker.js
@@ -1,1 +1,15 @@
+import { format } from "date-fns";
 import { useState } from "react";
+
+function DatePicker({ value, onChange }) {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="container">
+      <button>
+        {value == "null" ? "Select a Date" : format(value, "MM do,yyy")}
+      </button>
+      {isOpen && <DatePickerModal value={value} onChange={onChange} />}
+    </div>
+  );
+}
+export default DatePicker;
