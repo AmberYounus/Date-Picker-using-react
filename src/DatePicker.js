e057d3242c5adbc52891289327921e1db0ad04e7
Amber Younus <amberyounus22@gmail.com>
2024-03-10 11:05:12 -0700

    commit

--- src/DatePicker.js
+++ src/DatePicker.js
@@ -5,6 +5,9 @@ import {
   endOfMonth,
   endOfWeek,
   format,
+  isSameDay,
+  isSameMonth,
+  isToday,
   startOfMonth,
   startOfWeek,
 } from "date-fns";
@@ -29,44 +32,53 @@ function DatePickerModal({ value, onChange }) {
     end: endOfWeek(endOfMonth(visibleMonth)),
   });
 
-  //Show Previous MOnth 
+  //Show Previous MOnth
   function showPreviousMonth() {
-  setVisibleMonth(currentMonth => {
-    return addMonths(currentMonth, -1);
-  });
+    setVisibleMonth((currentMonth) => {
+      return addMonths(currentMonth, -1);
+    });
   }
 
-  //Show Next MOnth 
+  //Show Next MOnth
   function showNextMonth() {
-    setVisibleMonth(currentMonth => {
+    setVisibleMonth((currentMonth) => {
       return addMonths(currentMonth, +1);
     });
   }
 
-return (
- <>
-  <div className="date-picker">
-    <div className="header">
-      <button onClick={showPreviousMonth}>&larr;</button>
-    </div>
-    <div>{format(visibleMonth, "MMMM -yyyy")}</div>
-    <button onClick={showNextMonth}>&rarr;</button>
-    <div>
-      <div>Mon</div>
-      <div>Tues</div>
-      <div>wed</div>
-      <div>Thurs</div>
-      <div>Fri</div>
-      <div>Sat</div>
-      <div>Sun</div>
-    </div>
-    <div>
-      {visibleDates.map(date=>(
-        <button onClick={()=>onChange(date)} className="" key={date.toDataString()}>{date.getDate()}</button>
-      ))}
+  return (
+    <>
+      <div className="date-picker">
+        <div className="header">
+          <button onClick={showPreviousMonth}>&larr;</button>
         </div>
+        <div>{format(visibleMonth, "MMMM -yyyy")}</div>
+        <button onClick={showNextMonth}>&rarr;</button>
+        <div>
+          <div>Mon</div>
+          <div>Tues</div>
+          <div>wed</div>
+          <div>Thurs</div>
+          <div>Fri</div>
+          <div>Sat</div>
+          <div>Sun</div>
         </div>
-        </>
-);
-      }
+        <div>
+          {visibleDates.map((date) => (
+            <button
+              onClick={() => onChange(date)}
+              className={`date ${
+                  !isSameMonth(date, visibleMonth) && "date-picker-other-month-date"}
+                  ${isSameDay(date, value) && "selected"}
+                  ${isToday(date) && "today"}
+              }`}
+              key={date.toDataString()} >
+              {date.getDate()}
+            </button>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
 export default DatePicker;
